14c92ca3e9f312845cda7b8c93bbcb2be6e675ba
Wijesinghe W.N.U. <141928283+Niweesha543@users.noreply.github.com>
2024-11-21 13:47:31 +0530

    product adding component

---
+++ Type.ts
@@ -0,0 +1,12 @@
+export type Product = {
+    id: string;
+    name: string;
+    skuCode: string;
+    category: string;
+    brand: string;
+    description: string;
+    image: string;
+    price: number;
+    stock: number;
+    status: string;
+    };
